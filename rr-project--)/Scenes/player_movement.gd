d38992c384a3c06bd0d0a98274120743b166c1b2
LilGenius03 <finnarcher03@gmail.com>
2025-12-21 16:32:28 +0000

    camera fixes

--- rr-project--)/Scenes/player_movement.gd
+++ rr-project--)/Scenes/player_movement.gd
@@ -1,11 +1,17 @@
 extends CharacterBody3D
-const SENSITIVITY = 0.02
-const SPEED = 5.0
+const SENSITIVITY = 0.01
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 
 #bob variables
 const BOB_FREQ = 2
 const BOB_AMP = 0.08
 var t_bob = 0.0
+
+#FOV Variable
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
@@ -23,16 +29,19 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	
-	
+	#Handle Sprint
+	if Input.is_action_pressed("Sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
@@ -40,6 +49,10 @@ func _physics_process(delta: float) -> void:
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE *velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
 	move_and_slide()
 
