ca47361b1f31c96254c39cb54d75e01e23f8625e
LilGenius03 <finnarcher03@gmail.com>
2026-02-02 21:19:30 +0000

    holy shit what a pain

---
+++ rr-project--)/Scenes/statue.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+@onready var state_machine = $AnimationTree["parameters/playback"]
+
+func _ready():
+	$AnimationTree.active = true
+	state_machine.travel("Idle")
+	$AnimationTree.animation_finished.connect(_on_animation_tree_animation_finished)
+
+func _input(event):
+	if event.is_action_pressed("take_photo"):
+		state_machine.travel("Taking_photo")
+
+
+
+func _trigger_flash_and_change_scene():
+	var flash = $FlashOverlay
+	flash.visible = true
+	flash.modulate = Color.WHITE
+	flash.modulate.a = 0.0
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(flash, "modulate:a", 2.0, 0.5) #fade out
+	tween.tween_property(flash, "modulate:a", 1.0, 0.2) #fade in
+	
+	await tween.finished
+	get_tree().call_deferred("change_scene_to_file", "res://Scenes/first_date.tscn")    
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Taking_photo":
+		_trigger_flash_and_change_scene()
