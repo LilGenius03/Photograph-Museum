984bd777f8638e3320f1b3cd82f6045dd6296ad0
LilGenius03 <finnarcher03@gmail.com>
2026-02-12 18:39:45 +0000

    yep

---
+++ rr-project--)/Scenes/ValentinesDayCard.gd
@@ -0,0 +1,28 @@
+extends Area3D
+@export var Card: Control
+@export var PressF: Control
+var ReadingCard = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ReadingCard = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Inspect") && ReadingCard == false:
+		Card.visible = true
+		ReadingCard = true
+	elif Input.is_action_just_pressed("Inspect") && ReadingCard == true:
+			Card.visible = false
+			ReadingCard = false
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		PressF.visible = true
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.name == "Player":
+		PressF.visible = false
